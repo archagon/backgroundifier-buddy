3d484fec0948ce375e28d5ce9f5233271462387c
Archagon <archagon@gmail.com>
2018-04-21 15:53:39 -0700

    Created basic status bar menu.

--- BackgroundifierCompanion/AppDelegate.swift
+++ BackgroundifierCompanion/AppDelegate.swift
@@ -9,19 +9,41 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate
+{
+    var statusItem: NSStatusItem!
+    
+    enum MenuItem: Int
+    {
+        case item1 = 0
+        case item2
+        case separator1
     }
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification)
+    {
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        createMenu: do
+        {
+            let menu = NSMenu.init()
+            
+            menu.autoenablesItems = false
+            menu.delegate = self
+            
+            let item1 = NSMenuItem.init(title: "Item 1", action: nil, keyEquivalent: "e")
+            menu.addItem(item1)
+            assert(menu.items.count - 1 == MenuItem.item1.rawValue)
+            
+            let item2 = NSMenuItem.init(title: "Item 2", action: nil, keyEquivalent: "f")
+            menu.addItem(item2)
+            assert(menu.items.count - 1 == MenuItem.item2.rawValue)
+            
+            let sep1 = NSMenuItem.separator()
+            menu.addItem(sep1)
+            assert(menu.items.count - 1 == MenuItem.separator1.rawValue)
+            
+            self.statusItem.menu = menu
+        }
     }
-
-
 }
-

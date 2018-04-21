753d21bfebf64be064e9fbc09e3e1f4d77a296b0
Archagon <archagon@gmail.com>
2018-04-21 15:42:13 -0700

    Initial commit. Stub project.

---
+++ BackgroundifierCompanion/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BackgroundifierCompanion
+//
+//  Created by Alexei Baboulevitch on 2018-4-21.
+//  Copyright © 2018 Alexei Baboulevitch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
